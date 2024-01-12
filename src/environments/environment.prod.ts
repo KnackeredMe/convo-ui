afdd8a01ccaf674c6f351f884bb0447c22c7bf40
Vadym Marko <marko.vadym@gmail.com>
2024-01-12 14:24:36 +0200

    Firebase config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,12 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://online-shop.delightfulwater-f9bbc0d1.westeurope.azurecontainerapps.io/'
+  baseUrl: 'https://online-shop.delightfulwater-f9bbc0d1.westeurope.azurecontainerapps.io/',
+  firebaseConfig: {
+    apiKey: "AIzaSyAAiUgvrRA24dxWL9vQd5KImi1ATVKYD-U",
+    authDomain: "convo-a15c8.firebaseapp.com",
+    projectId: "convo-a15c8",
+    storageBucket: "convo-a15c8.appspot.com",
+    messagingSenderId: "264103664850",
+    appId: "1:264103664850:web:3108e0728a9f24f625610b"
+  }
 };
